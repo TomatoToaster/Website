f98cd743664a2e060983713a0d4bd9df27003ccc
Amal Nazeem <amalnazeem@gmail.com>
2018-11-11 07:29:18 -0500

    actually compiling scss to css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,45 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const APP_DIR = path.resolve(__dirname, 'dist');
 const SRC_DIR = path.resolve(__dirname, 'src');
 const PUB_DIR = path.resolve(__dirname, 'public');
 
 const config = {
-	entry: `${SRC_DIR}/index.js`,
-	output: {
-		path: APP_DIR,
-		filename: 'bundle.js',
-	},
-	mode: 'production',
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)?$/,
-				use: [
-							{
-								loader: 'babel-loader',
-									options: {
-										presets: ['react']
-									}
-							}
-				],
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    path: APP_DIR,
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+                presets: ['react']
+              }
+          }
+        ],
         exclude: /node_modules/
       },
       {
         test: /\.(s*)css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
         exclude: /node_modules/
-			},
+      },
     ],
-	},
-	resolve: {
-    extensions: ['.js', ''],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new HtmlWebPackPlugin({
@@ -44,10 +49,14 @@ const config = {
       template: `${SRC_DIR}/index.html`,
       filename: `${APP_DIR}/index.html`,
     }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
   ],
+
   devServer: {
-	contentBase: PUB_DIR,
-	},
+    contentBase: PUB_DIR,
+  },
 };
 
 module.exports = config;
