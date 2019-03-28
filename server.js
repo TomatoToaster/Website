f7dd3abd277957514020ebde45bd7a3705f0a8af
unknown <amalnazeem@gmail.com>
2019-03-28 04:48:40 -0400

    Added logic to support OneTwo game

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require('express')
 var path = require("path")
 var app = express()
 
+// sending up static assets
 app.use(express.static(__dirname + '/dist'))
 app.use(express.static(__dirname + '/public'))
+
+// sending up static files for OneTwo (duplicate index.html won't be a problem)
+app.use(express.static(path.resolve(__dirname, '..', 'OneTwo', 'dist')))
 
 // Set up Front End
 // =============================================================================
@@ -19,6 +23,11 @@ const front_end_routes = ['/', '/about', '/interests/:interest?'];
 
 // Now we are using the handler for each of the front end routes
 front_end_routes.forEach(route => app.get(route, front_end_handler));
+
+// Special route that will point to the game OneTwo
+app.get('/OneTwo', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '..', 'OneTwo', 'dist', 'index.html'))
+})
 
 app.listen(port, function() {
   console.log("Node app is running at localhost:" + port)
