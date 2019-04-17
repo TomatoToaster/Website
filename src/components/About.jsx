d146831bf30f79936983d946d091462432e84542
unknown <amalnazeem@gmail.com>
2019-04-17 13:19:36 -0400

    Changed facing email

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ class Stuff extends Component {
         <p>I am a Product Manager/Software Engineer that graduated from Northeastern University (December 2018) with a degree in Computer Science/Mathematics.</p>
         <p>This a personal website I made to review some of the concepts I've learned making websites. It sometimes acts as a playground, but it's meant to give a brief overview of who I am.</p>
         <h2>Contact Me</h2>
-        <p>Email: <a href='mailto:amalnazeem@gmail.com'>amalnazeem@gmail.com</a></p>
+        <p>Email: <a href='mailto:nazeem.a@husky.neu.edu'>nazeem.a@husky.neu.edu</a></p>
         <p>Phone: <a href='tel:5088474638'>508-847-4638</a></p>
       </Fragment>
     );
