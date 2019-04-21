399a484312f631736bc30e51ec489bad9c0d909a
unknown <amalnazeem@gmail.com>
2019-04-21 19:58:01 -0400

    Linked art in art page

--- src/components/Interests/Art.jsx
+++ src/components/Interests/Art.jsx
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
 import { Segment } from 'semantic-ui-react';
 
-class Art extends Component {
+const BACKGROUND_URLS = [
+  { 
+    url: 'https://raw.githubusercontent.com/TomatoToaster/Backgrounds/master/SunsetTomato/OceanSunsetTomato.gif',
+    altText: 'Ocean Sunset with a Tomato as the Sun',
+    isBig: true
+ }
+];
+const SPRITE_URLS = [
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/Sprites/master/Cat/CurlyTailCat.gif', altText: 'Curly Tail Cat' },
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/Sprites/master/Cat/HappyPenCat.gif', altText: 'Happy Pen Cat' },
+] 
+const DigitalArtPiece = ({url, altText, isBig = false}) => ( 
+  <img src={url} alt={altText} className={ isBig ? "big-digital-art-piece" : "digital-art-piece" } />
+); 
+
+class Art extends Component { 
   render() {
     return (
       <Segment>
         <h2>Pixel Art</h2>
         <p>Full disclaimer, I've only recently started learning how to make this, so I'm still very much an amateur!</p>
-        <a href={'https://github.com/TomatoToaster/Backgrounds.git'} target='_blank'>
-          <p>
-            Check out some of the backgrounds I've been working on.
-          </p>
-        </a>
-        <br/>
+
         <a href={'https://github.com/TomatoToaster/Sprites.git'} target='_blank'>
-          <p>
-            Or some of the sprites.
-          </p>
+          <p> Check out some of the sprites I've been working on: </p>
         </a>
+        <div className='pixel-sprites'>
+          {SPRITE_URLS.map(DigitalArtPiece)}
+        </div>
+
+        <br/>
+
+        <a href={'https://github.com/TomatoToaster/Backgrounds.git'} target='_blank'>
+          <p> Or some of the backgrounds: </p>
+        </a>
+        <div className='pixel-backgrounds'>
+          {BACKGROUND_URLS.map(DigitalArtPiece)}
+        </div>
       </Segment>
     );
   }
