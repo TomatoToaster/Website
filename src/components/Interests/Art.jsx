d492242e358a4f08a35124a4144c326a479f25ba
Amal Nazeem <amalnazeem@gmail.com>
2023-11-16 18:43:21 -0500

    Include more sprites in website

--- src/components/Interests/Art.jsx
+++ src/components/Interests/Art.jsx
@@ -2,20 +2,28 @@ import React, { Component, Fragment } from 'react';
 
 const BACKGROUND_URLS = [
   {
-    urlSmall: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/master/Backgrounds/SunsetTomato/OceanSunsetTomatoRaw.gif',
-    urlBig: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/master/Backgrounds/SunsetTomato/OceanSunsetTomato.gif',
+    urlSmall: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Backgrounds/SunsetTomato/OceanSunsetTomatoRaw.gif',
+    urlBig: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Backgrounds/SunsetTomato/OceanSunsetTomato.gif',
     altText: 'Ocean Sunset with a Tomato as the Sun',
   },
   {
-    urlSmall: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/master/Backgrounds/Rain/Cat_In_Rain_Raw.gif',
-    urlBig: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/master/Backgrounds/Rain/Cat_In_Rain.gif',
+    urlSmall: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Backgrounds/Rain/Cat_In_Rain_Raw.gif',
+    urlBig: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Backgrounds/Rain/Cat_In_Rain.gif',
     altText: 'Cat sitting outside in the rain',
   },
 ];
 const SPRITE_URLS = [
-  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/master/Sprites/Cat/CurlyTailCat.gif', altText: 'Curly Tail Cat' },
-  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/master/Sprites/Flower/DancingFlower.gif', altText: 'Dancing Flower' },
-  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/master/Sprites/Cat/HappyPenCat.gif', altText: 'Happy Pen Cat' },
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Sprites/Cat/CurlyTailCat.gif', altText: 'Curly Tail Cat' },
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Sprites/Flower/DancingFlower.gif', altText: 'Dancing Flower' },
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Sprites/Cat/HappyPenCat.gif', altText: 'Happy Pen Cat' },
+]
+
+const SPRITE_URLS2 = [
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Whole/FreazyPeazy/Peazy.gif', altText: 'Peazy' },
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Whole/RainBee/BeeEater.gif', altText: 'Bee Eater (a bird)' },
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Whole/CatCopyPaste/PJ.gif', altText: 'PJ from Cat Copy Paste' },
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Whole/RainBee/Bee.gif', altText: 'Bee' },
+  { url: 'https://raw.githubusercontent.com/TomatoToaster/PixelArt/main/Whole/RainBee/Queen.gif', altText: 'Queen Bee' },
 ]
 const DigitalArtPiece = ({url, altText}) => (
   <img src={url} alt={altText} className="digital-art-piece" />
@@ -35,16 +43,20 @@ class Art extends Component {
         <h2>Pixel Art</h2>
         <p>I'm an amateur in the art stuff don't judge :)</p>
 
-        <a href={'https://github.com/TomatoToaster/PixelArt/tree/master/Sprites'} target='_blank'>
+        <a href={'https://github.com/TomatoToaster/PixelArt/tree/main/Sprites'} target='_blank'>
           Check out some of the sprites I've been working on:
         </a>
         <div className='pixel-sprites'>
           {SPRITE_URLS.map(DigitalArtPiece)}
         </div>
 
+        <div className='pixel-sprites'>
+          {SPRITE_URLS2.map(DigitalArtPiece)}
+        </div>
+
         <br/>
 
-        <a href={'https://github.com/TomatoToaster/PixelArt/tree/master/Backgrounds'} target='_blank'>
+        <a href={'https://github.com/TomatoToaster/PixelArt/tree/main/Backgrounds'} target='_blank'>
           Or some of the backgrounds:
         </a>
         <div className='pixel-backgrounds'>
