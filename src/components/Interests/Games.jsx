d696f4fdbce294aad98ef7d81efdf6e98f9e1ea0
Amal Nazeem <amalnazeem@gmail.com>
2024-11-22 01:33:58 -0500

    Update and add new game

--- src/components/Interests/Games.jsx
+++ src/components/Interests/Games.jsx
@@ -7,6 +7,7 @@ class Games extends Component {
                 <h2>Video Games</h2>
                 <div className='game-preview'>
                 <p>As featured on my <a href={'https://tomatotoaster.itch.io'}>itch.io</a>:</p>
+                <p><iframe frameborder="0" src="https://itch.io/embed/3110297?bg_color=7d7071&amp;fg_color=302c2e&amp;link_color=e6482e&amp;border_color=615973" width="552" height="167"><a href="https://tomatotoaster.itch.io/rainbee">Rainbee (Demo) by TomatoToaster</a></iframe></p>
                 <p>
                 <iframe className='itchFrame' width="552" height="167" frameborder="0" src="https://itch.io/embed/2333531?dark=true"><a href="https://tomatotoaster.itch.io/video-room">Video Room by TomatoToaster</a></iframe>
                 </p>
